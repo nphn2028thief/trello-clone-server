76c8ce8813a3a0335b40f382de1a41c188409a5e
Nguyen Phan Hoai Nhan <nphn2082thief@gmail.com>
2024-02-18 19:24:59 +0700

    Implement get board by id and organization id api

--- app/routes/Board.ts
+++ app/routes/Board.ts
@@ -5,6 +5,10 @@ import BoardController from "../controllers/BoardController";
 const boardRoutes = (router: Router) => {
   router.post("/board", BoardController.createBoard);
   router.get("/boards/:organizationId", BoardController.getBoardsByOrgId);
+  router.get(
+    "/boards/:organizationId/:boardId",
+    BoardController.getBoardByOrgIdAndBoardId
+  );
 };
 
 export default boardRoutes;
